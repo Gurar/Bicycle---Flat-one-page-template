6d52ecbc200db9b357f79d294a946caf0a7ff2a6
Gurar <jozyk@inbox.lt>
2020-11-07 20:58:54 +0200

    burger elemet fixed

--- #src/js/main.js
+++ #src/js/main.js
@@ -8,11 +8,11 @@ if(menu) {
     menu.addEventListener('click', (e) => {
         e.preventDefault();
         menu.classList.toggle('active');
-        document.querySelector('.navbar__list--collapse').classList.toggle('menu-open');
+        document.querySelector('.nav--collapse').classList.toggle('menu-open');
         window.addEventListener('click', (event) => {
             if(event.target.tagName.toLowerCase() === 'a') {
                 menu.classList.remove('active');
-                document.querySelector('.navbar__list--collapse').classList.remove('menu-open');
+                document.querySelector('.nav--collapse').classList.remove('menu-open');
         }
         });
     });
